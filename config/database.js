de9d744a33c6ef10f49bf3590a865c273d60e702
C9-LinkRs <linkrsfan@gmail.com>
2020-03-30 01:52:57 +0200

    heroku changes

--- config/database.js
+++ config/database.js
@@ -12,9 +12,9 @@ const OPTIONS = {
   useUnifiedTopology: true,
   connectTimeoutMS: 10000
 };
-const URL = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_SERVER}/${MONGODB_DATABASE}?authSource=admin`;
+const URL = "mongodb://localhost/asistencias-un-prod";
 
-mongoose.connect(URL, OPTIONS).then(() => {
+mongoose.connect(process.env.MONGODB_URI || URL, OPTIONS).then(() => {
   console.log("MongoDB connection successful");
 }).catch(error => {
   console.log("MongoDB connection error", error);
